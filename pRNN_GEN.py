e386df2a815330323249e61c06f745eeb08f97e0
fabienfrfr <69802483+fabienfrfr@users.noreply.github.com>
2021-01-10 15:57:19 +0100

    Update pRNN_GEN.py

--- pRNN_GEN.py
+++ pRNN_GEN.py
@@ -8,6 +8,8 @@ Created on Sun Dec  6 14:01:21 2020
 import torch, torch.nn as nn
 import numpy as np
 
+TEST_CLASS = True
+
 ################################ Custom neural network
 class pRNN(nn.Module):
     def __init__(self, Net, batch_size):
@@ -16,28 +18,64 @@ class pRNN(nn.Module):
         # list of layers
         self.Layers = nn.ModuleList([nn.Sequential(nn.Linear(n[2], n[1]), nn.ReLU()) for n in self.Net])
         self.trace = [torch.zeros(batch_size,n[1]) for n in self.Net]
+        # input memory
+        self.input = None
         # virtual trace (pseudo-rnn)
         self.h = None
 
     def forward(self,x):
+        self.input = x
         # virtualization (t-1)
         self.h = [t.detach() for t in self.trace]
         # BP follow XY position
         order = np.argsort(self.Net[:, 3])
         for i in range(self.Net.shape[0]) :
             idx = order[i]
-            # input
-            if i == 0 : x = self.Layers[idx](x)
-            # hidden + output
-            else :
-                tensor = []
-                for j,k in self.Net[idx, -1] :
+            tensor = []
+            for j,k in self.Net[idx, -1] :
+                # input
+                if j == 0 : tensor += [self.input[:,None,k]]
+                # hidden
+                else :
                     idx_ = np.where(self.Net[:,0] == j)[0][0]
                     # pseudo-RNN
                     if (self.Net[idx_, 3] >= self.Net[idx, 3]) : tensor += [self.h[idx_][:,None,k]]
                     # Non Linear input
                     else : tensor += [self.trace[idx_][:,None,k]]
-                tensor_in = torch.cat(tensor, dim=1)
-                x = self.Layers[idx](tensor_in)
+            tensor_in = torch.cat(tensor, dim=1)
+            x = self.Layers[idx](tensor_in)
             self.trace[idx] = x
         return x
+
+################################ GRAPH TESTER
+if TEST_CLASS :
+    from GRAPH_GEN import GRAPH
+    # Generator
+    NB_P_GEN = 16
+    P_MIN = 1
+    
+    # I/O
+    I = 16 # image cells
+    O = 16 # action
+    
+    NET = GRAPH(NB_P_GEN, I, O, P_MIN)
+    Net = NET.NEURON_LIST
+    print("Liste des neurons : \n", Net)
+    
+    batch_size = 5
+    # IO values
+    X = np.mgrid[0:batch_size,0:I][1]
+    y = 1*np.logical_xor(X < 3, X > 7)
+    X, y = torch.tensor(X, dtype=torch.float), torch.tensor(y, dtype=torch.float)
+    print("XOR output (not shuffleled..) : \n", y)
+    
+    # init RNN
+    h0 = torch.zeros(batch_size,1).requires_grad_()
+
+    # Model init
+    model = pRNN(Net, batch_size)
+    print("Model : \n", model)
+    
+    # test prediction
+    y_pred = model(X)
+    print("first output predicted : \n", y_pred)
