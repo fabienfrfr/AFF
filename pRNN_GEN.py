d8fe9133e4d3c798fd7797f150a8b0189961b45b
fabienfrfr <69802483+fabienfrfr@users.noreply.github.com>
2021-03-18 13:47:01 +0100

    Update pRNN_GEN.py

--- pRNN_GEN.py
+++ pRNN_GEN.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec  6 14:01:21 2020
+Created on Tue Mar 16 17:01:37 2021
 @author: fabien
 """
 
@@ -9,40 +9,44 @@ import torch, torch.nn as nn
 import numpy as np
 
 ################################ Custom neural network
-class pRNN(nn.Module):
-    def __init__(self, Net, batch_size):
-        super().__init__()
-        self.Net = Net
-        # list of layers
-        self.Layers = nn.ModuleList([nn.Sequential(nn.Linear(n[2], n[1]), nn.ReLU()) for n in self.Net])
-        self.trace = [torch.zeros(batch_size,n[1]) for n in self.Net]
-        # input memory
-        self.input = None
-        # virtual trace (pseudo-rnn)
-        self.h = None
 
+class pRNN(nn.Module):
+    def __init__(self, NET,B,I):
+        super().__init__()
+        self.NET = NET
+        self.BS = B
+        # list of layers
+        self.Layers = nn.ModuleList( [nn.Sequential(nn.Linear(n[2], n[1]), nn.ReLU()) for n in self.NET] +
+                                     [nn.Sequential(nn.Conv1d(I, I, 1, groups=I, bias=True), nn.ReLU())])
+        # trace data
+        self.trace = (NET.shape[0]+1)*[None]
+        # pseudo RNN (virtual input)
+        self.h = [torch.zeros(B,n[1]) for n in self.NET] + [torch.zeros(B,I)]
+        
     def forward(self,x):
-        self.input = x
+        s = x.shape
         # Generalization of Exploitation or Training batch
-        BATCH_ = np.arange(len(x)) # min = 1, max = batch_size 
-        # virtualization (t-1)
-        self.h = [t.detach() for t in self.trace]
-        # BP follow XY position
-        order = np.argsort(self.Net[:, 3])
-        for i in range(self.Net.shape[0]) :
-            idx = order[i]
+        if s == self.BS : BATCH_ = slice(None)
+        elif s == 1 : BATCH_ = 0
+        else : BATCH_ = np.arange(len(x))
+        # input functionalization (with spread sparsing)
+        self.trace[-1] = self.Layers[-1](x.view(s[0],s[1],1)).view(s)
+        # hidden to output (X ordered)
+        for i in np.argsort(self.NET[:, 3]) :
             tensor = []
-            for j,k in self.Net[idx, -1] :
+            for j,k in self.NET[i, -1] :
                 # input
-                if j == 0 : tensor += [self.input[:,None,k]]
+                if j == 0 : tensor += [self.trace[-1][BATCH_,None,k]]
                 # hidden
                 else :
-                    idx_ = np.where(self.Net[:,0] == j)[0][0]
-                    # pseudo-RNN
-                    if (self.Net[idx_, 3] >= self.Net[idx, 3]) : tensor += [self.h[idx_][BATCH_,None,k]]
+                    # pseudo-RNN (virtual input)
+                    if (self.NET[i, 3] >= self.NET[i, 3]) : tensor += [self.h[j][BATCH_,None,k]]
                     # Non Linear input
-                    else : tensor += [self.trace[idx_][BATCH_,None,k]]
+                    else : tensor += [self.trace[j][BATCH_,None,k]]
             tensor_in = torch.cat(tensor, dim=1)
-            x = self.Layers[idx](tensor_in)
-            self.trace[idx][BATCH_] = x
-        return x
+            self.trace[i] = self.Layers[i](tensor_in)
+        # save for t+1
+        for t in range(len(self.trace)):
+            self.h[t][BATCH_] = self.trace[t][BATCH_].detach()
+        # output
+        return self.trace[i]
