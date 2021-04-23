b2b8519d87f11599257dbc1c0731b98dca259e46
fabienfrfr <69802483+fabienfrfr@users.noreply.github.com>
2021-04-23 20:22:41 +0200

    Update Q_AGENT_GEN.py

--- Q_AGENT_GEN.py
+++ Q_AGENT_GEN.py
@@ -5,32 +5,40 @@ Created on Tue Jan 26 11:38:14 2021
 @author: fabien
 """
 import torch, torch.nn as nn
-import torch.optim as optim
 import numpy as np
 
 from GRAPH_EAT import GRAPH_EAT
 from pRNN_GEN import pRNN
 
-################################ GRAPH of Network
+################################ AGENT
 class Q_AGENT():
-    def __init__(self, NB_P_GEN, IO, BS):
+    def __init__(self, NB_P_GEN, IO, BS, NET = None, COOR = None):
         self.P_MIN = 1
-        self.P_DILEMNA = 0.4*np.random.random() + 0.1
+        self.NB_P_GEN = NB_P_GEN
         # I/O
         self.IO = IO # image cells, action
         self.batch_size = BS
         ## Init
-        self.NET = GRAPH_EAT([NB_P_GEN, self.IO[0], self.IO[1], self.P_MIN], None)
+        if NET == None :
+            self.NET = GRAPH_EAT([NB_P_GEN, self.IO[0], self.IO[1], self.P_MIN], None)
+        else :
+            self.NET = NET
         self.NEURON_LIST = self.NET.NEURON_LIST
-        self.MODEL = pRNN(self.NEURON_LIST, self.batch_size)
+        self.MODEL = pRNN(self.NEURON_LIST, self.batch_size, self.IO[0])
         # nn optimiser
         self.GAMMA = 0.9
-        self.optimizer = optim.Adam(self.MODEL.parameters())
-        self.criterion = nn.MSELoss()
+        #self.optimizer = torch.optim.Adam(self.MODEL.parameters())
+        self.optimizer = torch.optim.SGD(self.MODEL.parameters(), lr=1e-6)
+        self.criterion = nn.MSELoss(reduction='sum')
+        self.loss = None
         ## IO Coordinate
-        self.X,self.Y = self.FIRST_IO_COOR_GEN()
+        if COOR == None :
+            self.X,self.Y = self.FIRST_IO_COOR_GEN()
+        else :
+            self.X,self.Y = COOR
         ## Data sample (memory : 'old_state', 'action', 'new_state', 'reward', 'terminal')
         self.MEMORY = [[],[],[],[],[]]
+        self.MEMORY_ = None
     
     ## Define coordinate of IN/OUT
     def FIRST_IO_COOR_GEN(self) :
@@ -45,23 +53,19 @@ class Q_AGENT():
         y = np.random.choice(range(len(Y)), self.IO[1], replace=False)
         # note for output min : cyclic 3,4 if 3 mvt, 2 if 4 mvt
         return X[x], Y[y]
-    
+
     ## Action Exploration/Exploitation Dilemna
     def ACTION(self, Input) :
         img_in = torch.tensor(Input, dtype=torch.float)
-        DEAL = True #np.random.choice([True,False], 1, p=[self.P_DILEMNA, 1 - self.P_DILEMNA])[0]
-        if DEAL :
-            # prnn buffer
-            output = self.MODEL(img_in)
-            # exploration
+        # actor-critic
+        action_probs, critic_value = self.MODEL(img_in)
+        # exploration-exploitation dilemna
+        DILEMNA = np.squeeze(action_probs.detach().numpy())
+        if DILEMNA.sum() == 0 or str(DILEMNA.sum()) == 'nan' :
             next_action = np.random.randint(self.IO[1])
-        else :
-            # exploitation
-            output = self.MODEL(torch.from_numpy(img_in).float())
-            # Max values classification
-            _, predicted = torch.max(output.data, 1)
-            predicted = predicted.numpy()
-            next_action = predicted[0]
+        else : 
+            p_norm = DILEMNA/DILEMNA.sum()
+            next_action = np.random.choice(self.IO[1], p=p_norm)
         return next_action
     
     ## Memory sequencing
@@ -82,15 +86,32 @@ class Q_AGENT():
     def OPTIM(self) :
         # extract info
         old_state, action, new_state, reward, DONE = self.MEMORY
+        # actor-critic
+        actor, critic = self.MODEL(old_state)
         # Compute predicted Q-values for each action
-        pred_q_values_batch = torch.sum(self.MODEL(old_state).gather(1, action),dim=1).detach()
-        pred_q_values_next = self.MODEL(new_state)
+        pred_q_values_batch = torch.sum(actor.gather(1, action),dim=1).detach()
+        pred_q_values_next, critic  = self.MODEL(new_state)
         # Compute targeted Q-value for action performed
         target_q_values_batch = reward+(1-DONE)*self.GAMMA*torch.max(pred_q_values_next, 1)[0]
         # zero the parameter gradients
         self.MODEL.zero_grad()
         # Compute the loss
-        loss = self.criterion(pred_q_values_batch,target_q_values_batch)
+        self.loss = self.criterion(pred_q_values_batch,target_q_values_batch)
         # Do backward pass
-        loss.backward()
+        self.loss.backward()
         self.optimizer.step()
+        # reset memory
+        self.MEMORY_ = self.MEMORY
+        self.MEMORY = [[],[],[],[],[]]
+    
+    ## reset object
+    def RESET(self):
+        GRAPH = self.NET.NEXT_GEN(-1)
+        XY_TUPLE = (self.X,self.Y)
+        return Q_AGENT(self.NB_P_GEN, self.IO, self.batch_size, NET = GRAPH, COOR = XY_TUPLE)
+    
+    ## mutation
+    def LAUNCH_MUTATION(self, MUT = None):
+        GRAPH = self.NET.NEXT_GEN(MUT)
+        XY_TUPLE = (self.X,self.Y)
+        return Q_AGENT(self.NB_P_GEN, self.IO, self.batch_size, NET = GRAPH, COOR = XY_TUPLE)
